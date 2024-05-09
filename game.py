5bc8b15eee888eb8de845c2804ab0aed101ea38e
clelievn <147336693+clelievn@users.noreply.github.com>
2024-05-09 18:54:45 +0200

    Update game.py

--- game.py
+++ game.py
@@ -10,20 +10,21 @@ class Game:
             tower = Tower()
         self.tower = tower
 
-    def play(self, position: tuple[int, int], new_position: tuple[int, bool]) -> None:
-        """Plays a block from a row/position to a new position"""
-        # TODO: add dry_run to return the new tower without modifying the current one
-
-        x, y = position
+    def fresh_play(self, position: tuple[int, int], new_position: tuple[int, bool]) -> None:
+        """Plays a block from a row/position to a new position and returns the new tower without modifying the current tower"""
+        x, z = position
         new_x, create_new_row = new_position
         tower_height = len(self.tower.layout)
+
+        new_layout = self.tower.layout.copy()
+        new_tower = Tower(new_layout)
 
         if x not in [LEFT, MID, RIGHT]:
             raise ValueError(f"invalid position: no such position. Must be in [{LEFT}, {MID}, {RIGHT}]")
 
-        if y == -1:
-            y = tower_height - 1
-        if y < 0 or y >= len(self.tower.layout) - 1:
+        if z == -1:
+            z = tower_height - 1
+        if z < 0 or z >= len(self.tower.layout) - 1:
             raise ValueError(f"invalid position: no such row. Must be in [0, {tower_height - 2}]")
 
         if new_x not in [LEFT, MID, RIGHT]:
@@ -37,7 +38,44 @@ class Game:
                 raise ValueError(f"invalid new_position: already occupied")
 
         # take jenga piece
-        self.tower[y][x] = False
+        new_tower[z][x] = False
+
+        # put it back on top
+        if create_new_row:
+            new_layout.append([False, False, False])
+        new_tower[-1][new_x] = True
+
+        return new_tower
+
+
+
+    def play(self, position: tuple[int, int], new_position: tuple[int, bool]) -> None:
+        """Plays a block from a row/position to a new position"""
+
+        x, z = position
+        new_x, create_new_row = new_position
+        tower_height = len(self.tower.layout)
+
+        if x not in [LEFT, MID, RIGHT]:
+            raise ValueError(f"invalid position: no such position. Must be in [{LEFT}, {MID}, {RIGHT}]")
+
+        if z == -1:
+            z = tower_height - 1
+        if z < 0 or z >= len(self.tower.layout) - 1:
+            raise ValueError(f"invalid position: no such row. Must be in [0, {tower_height - 2}]")
+
+        if new_x not in [LEFT, MID, RIGHT]:
+            raise ValueError(f"invalid new_position: no such row. Must be in [{LEFT}, {MID}, {RIGHT}]")
+
+        if create_new_row and sum(self.tower.layout[-1]) != 3:
+            raise ValueError(f"invalid new_position: cannot create new row when the last row is not full")
+
+        if not create_new_row:
+            if self.tower.layout[-1][new_x] == True:
+                raise ValueError(f"invalid new_position: already occupied")
+
+        # take jenga piece
+        self.tower[z][x] = False
 
         # put it back on top
         if create_new_row:
